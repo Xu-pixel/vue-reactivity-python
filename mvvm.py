31f51cfc819f7f7705291912a42902f8995cff9a
Xu-pixel <naomi1299@163.com>
2022-07-23 13:14:32 +0800

    完善的响应式

--- mvvm.py
+++ mvvm.py
@@ -1,39 +1,71 @@
+from collections import defaultdict
 from dataclasses import dataclass
 
-bucket = set()
-activeEffect  = None
-
-def effect(fn):
-    global activeEffect
-    activeEffect = fn
-    fn()
+bucket = defaultdict(lambda: defaultdict(set))
+activeEffect = None
+effect2deps = defaultdict(list)  # list存放与副作用相关的依赖集合
 
 
 class Proxy(dict):
-    def __init__(self,target) -> None:
+    def __init__(self, target) -> None:
         self['target'] = target
-    
+
     def __setattr__(self, __name: str, __value) -> None:
-        setattr(self['target'],__name,__value)
-        for fn in bucket:
-            fn()
+        setattr(self['target'], __name, __value)
+        trigger(self['target'], __name)
 
     def __getattribute__(self, __name: str):
-        if activeEffect: bucket.add(activeEffect)
-        return getattr(self['target'],__name)
+        track(self['target'], __name)
+        return getattr(self['target'], __name)
 
+
+def effect(fn):
+    def effectFn():
+        global activeEffect
+        cleanup(effectFn)
+        activeEffect = effectFn
+        fn()
+        activeEffect = None
+
+    effectFn()
+
+
+def track(target, key):
+    if len(key) >= 2 and key[:2] == '__':
+        return  # 内置变量不要追踪
+
+    deps = bucket[id(target)][key]
+    deps.add(activeEffect)
+    effect2deps[activeEffect].append(deps)
+
+
+def trigger(target, key):
+    if len(key) >= 2 and key[:2] == '__':
+        return
+    effects = bucket[id(target)][key]
+    effectsToRun = {effect for effect in effects if effect != activeEffect}
+    for effect in effectsToRun:
+        effect()
+
+
+def cleanup(effectFn):
+    for deps in effect2deps[effectFn]:
+        deps.remove(effectFn)
+    del effect2deps[effectFn]
 
 
 if __name__ == '__main__':
     @dataclass
     class Person:
-        name:str
-        age:int
+        name: str
+        age: int
 
-    data = Person('wei',21)
 
+    data = Person('wei', 21)
     obj = Proxy(data)
-    print(obj)
-    def fn():
-        print(obj)
-    effect(fn)
+    effect(lambda: (print(obj.name) if obj.age else None))
+    obj.age = 0
+    obj.age = 1
+    obj.age = 0
+    obj.age = 0
+    obj.age = 0
